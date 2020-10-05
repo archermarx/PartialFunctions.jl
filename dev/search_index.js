42ae26928ea970f7755526288ea336643be67cff
Documenter.jl <documenter@juliadocs.github.io>
2020-10-05 23:17:50 +0000

    build based on 8ba162f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PartialFunctions","category":"page"},{"location":"#PartialFunctions","page":"Home","title":"PartialFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PartialFunctions]","category":"page"}]
+}
