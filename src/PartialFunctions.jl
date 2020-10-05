8ba162faa693d28569af4f4154e3303da67e4c8d
Thomas Marks <marksta@umich.edu>
2020-10-05 19:13:13 -0400

    Initial commit

--- src/PartialFunctions.jl
+++ src/PartialFunctions.jl
@@ -1,5 +1,31 @@
 module PartialFunctions
 
-# Write your package code here.
+export $
+
+name(x) = (string ∘ Symbol)(x)
+
+struct PartialFunction{F<:Function, T<:Tuple} <: Function
+    func::F
+    args::T
+    PartialFunction(f::F, args::T) where {F<:Function, T<:Tuple} = new{F, T}(f, args)
+    function PartialFunction(f::PartialFunction, newargs::T) where T<:Tuple
+        allargs = (f.args..., newargs...)
+        new{typeof(f.func), typeof(allargs)}(f.func, allargs)
+    end
+end
+
+(p::PartialFunction)(newargs...) = p.func(p.args..., newargs...)
+
+($)(f::Function, args...) = PartialFunction(f, args)
+($)(f::Function, arg) = PartialFunction(f, (arg,))
+
+function Base.Symbol(pf::PartialFunction)
+    func_name = name(pf.func)
+    argstring = "(" * join(repr.(pf.args), ", ") *", ...)"
+    Base.Symbol("$(func_name)$(argstring)")
+end
+
+Base.show(io::IO, pf::PartialFunction) = print(io, name(pf))
+Base.show(io::IO, ::MIME"text/plain", pf::PartialFunction) = show(io, pf)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using PartialFunctions
 using Test
 
+a(x) = x^2
+greet(greeting, name, punctuation) = "$(greeting), $(name)$(punctuation)"
 @testset "PartialFunctions.jl" begin
-    # Write your tests here.
+    @test map((+)$2, [1,2,3]) == [3, 4, 5]
+    @test repr(map $ a) == "map(a, ...)"
+    @test (map $ a)([1, 2, 3]) == [1, 4, 9]
+    
+    @test greet("Hello", "Bob", "!") == "Hello, Bob!"
+    sayhello = greet $ "Hello"
+    @test repr(sayhello) == "greet(\"Hello\", ...)"
+    @test sayhello("Bob", "!") == "Hello, Bob!"
+    hi_bob = greet $ "Hi" $ "Bob" $ "!"
+    @test hi_bob isa PartialFunctions.PartialFunction{typeof(greet), Tuple{String, String, String}}
 end
